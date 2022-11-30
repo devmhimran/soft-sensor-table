ae8ad19122b79c650ce24a90fa5f3e1083dc8922
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-11-30 23:43:42 +0600

    Added Sort

--- soft-sensor-table/src/Component/Table/Table.jsx
+++ soft-sensor-table/src/Component/Table/Table.jsx
@@ -1,33 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import { HiOutlineArrowsUpDown } from 'react-icons/hi2';
+import tableTestData from '../../../public/tableTestData.json'
 
 const Table = ({ exceptName, exceptCity, exceptEmail, exceptJoin, exceptRole }) => {
     const [tableData, setTableData] = useState([]);
-
+    const [orderData, setOrderData] = useState("ASC");
     useEffect(() => {
         fetch('tableTestData.json')
             .then(res => res.json())
             .then(data => setTableData(data))
-    }, [])
+    }, [orderData])
+
+    const handleSort = (col) => {
+        if (orderData === "ASC") {
+            const sortedData = [...tableData].sort((a, b) =>
+                a[col] > b[col] ? 1 : -1
+            )
+            setTableData(sortedData)
+            setOrderData("DSC")
+        }
+        if (orderData === "ASC") {
+            const sortedData = [...tableData].sort((a, b) =>
+                a[col] < b[col] ? 1 : -1
+            )
+            setTableData(sortedData)
+            setOrderData("ASC")
+        }
+        console.log(col)
+
+    }
 
     return (
         <div className='container mx-auto flex justify-center py-16'>
             <table className='w-4/6'>
                 <thead>
                     <tr className='border'>
-                        <th className={`border text-left py-2 px-4 ${exceptName === 'show' ? '' : 'hidden'}`}>
+                        <th className={`border text-left py-2 px-4 cursor-pointer ${exceptName === 'show' ? '' : 'hidden'}`} onClick={() => handleSort('name')}>
                             <span className='flex items-center'>Name <HiOutlineArrowsUpDown className='ml-2' /></span>
                         </th>
-                        <th className={`border text-left py-2 px-4 ${exceptCity === 'show' ? '' : 'hidden'}`}>
+                        <th className={`border text-left py-2 px-4 cursor-pointer ${exceptCity === 'show' ? '' : 'hidden'}`} onClick={() => handleSort('city')}>
                             <span className='flex items-center'>City <HiOutlineArrowsUpDown className='ml-2' /></span>
                         </th>
-                        <th className={`border text-left py-2 px-4 ${exceptEmail === 'show' ? '' : 'hidden'}`}>
+                        <th className={`border text-left py-2 px-4 cursor-pointer ${exceptEmail === 'show' ? '' : 'hidden'}`} onClick={() => handleSort('email')}>
                             <span className='flex items-center'>Email Address <HiOutlineArrowsUpDown className='ml-2' /></span>
                         </th>
-                        <th className={`border text-left py-2 px-4 ${exceptJoin === 'show' ? '' : 'hidden'}`}>
+                        <th className={`border text-left py-2 px-4 cursor-pointer ${exceptJoin === 'show' ? '' : 'hidden'}`} onClick={() => handleSort('joiningDate')}>
                             <span className='flex items-center'>Joining Date <HiOutlineArrowsUpDown className='ml-2' /></span>
                         </th>
-                        <th className={`border text-left py-2 px-4 ${exceptRole === 'show' ? '' : 'hidden'}`}>
+                        <th className={`border text-left py-2 px-4 cursor-pointer ${exceptRole === 'show' ? '' : 'hidden'}`} onClick={() => handleSort('role')}>
                             <span className='flex items-center'>Role <HiOutlineArrowsUpDown className='ml-2' /></span>
                         </th>
                     </tr>
@@ -36,9 +56,14 @@ const Table = ({ exceptName, exceptCity, exceptEmail, exceptJoin, exceptRole }) 
                     {
                         tableData.map((data, index) =>
                             <tr className='border'>
-                                <td className={`${index % 2 == 0 ? 'active' : ''} ${exceptName === 'show' ? '' : 'hidden'}`}>{data.person.name}</td>
+                                <td className={`${index % 2 == 0 ? 'active' : ''} ${exceptName === 'show' ? '' : 'hidden'}`}>
+                                    <span className='flex items-center gap-3'>
+                                        <img src={data.person.avatar} alt="" />
+                                        {data.person.name}
+                                    </span>
+                                </td>
                                 <td className={`${index % 2 == 0 ? 'active' : ''} ${exceptCity === 'show' ? '' : 'hidden'}`}>{data.city}</td>
-                                <td className={`${index % 2 == 0 ? 'active' : ''} ${exceptEmail === 'show' ? '' : 'hidden'}`}>{data.email}</td>
+                                <td className={`${index % 2 == 0 ? 'active' : ''} ${exceptEmail === 'show' ? '' : 'hidden'} underline text-blue-500`}> <a href={`mailto:${data.email}`}>{data.email}</a> </td>
                                 <td className={`${index % 2 == 0 ? 'active' : ''} ${exceptJoin === 'show' ? '' : 'hidden'}`}>{data.joiningDate}</td>
                                 <td className={`${index % 2 == 0 ? 'active' : ''} ${exceptRole === 'show' ? '' : 'hidden'}`}>{data.role}</td>
                             </tr>
